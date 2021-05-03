50339aa161870ee62212892b5c99987adf702f78
Albert Chen <albert.chen@me.com>
2021-05-03 16:00:30 +0800

    chore: init repo

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const FACTORY_CONTRACT_ADDRESS =
+  process.env.REACT_APP_FACTORY_CONTRACT_ADDRESS ||
+  '0x1E90957F16D9E9ffaD9e73b612aF5EAb7a5bAb3e';
+
+export const TOKEN_CONTRACT_ADDRESS =
+  process.env.REACT_APP_TOKEN_CONTRACT_ADDRESS ||
+  '0x676855097117a6a32043db65c046d53b72e72b04';
+
+export const EXPLORER_URL =
+  process.env.REACT_APP_EXPLORER_URL || 'https://bscscan.com';
+
+export const REQUIRED_CHAIN_ID = process.env.REQUIRED_CHAIN_ID || '0x01';
